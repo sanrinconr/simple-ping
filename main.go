fba55c2c4ba64a150fe7af596ad99b289f186ee9
Santiago Rincon <santiagorincon2001@gmail.com>
2024-11-17 11:42:25 -0500

    Expose prometheus service and create counter

--- main.go
+++ main.go
@@ -4,13 +4,31 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
 	s := http.NewServeMux()
+
+	scope := "local"
+	if customScope := os.Getenv("SCOPE"); customScope != "" {
+		scope = customScope
+	}
+
+	counter := promauto.NewCounter(prometheus.CounterOpts{
+		Name: "simple_ping_request_done",
+		ConstLabels: map[string]string{
+			"scope": scope,
+		},
+	})
+
+	s.Handle("/metrics", promhttp.Handler())
 	s.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		counter.Inc()
 		w.Write([]byte("pong"))
-		fmt.Print(".")
 	})
 
 	port := "8080"
@@ -22,8 +40,6 @@ func main() {
 	locationKey := os.Getenv("KEY_FILE_LOCATION")
 
 	fmt.Printf("Running in port %s!\n", port)
-	fmt.Println(locationCertificate)
-	fmt.Println(locationKey)
 
 	if err := http.ListenAndServeTLS(":"+port, locationCertificate, locationKey, s); err != nil {
 		fmt.Println(err)
